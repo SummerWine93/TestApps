d41215eb780d93c768166287e55993b3590c6dcf
Anastasiya Yarovenko <anastasijajarvenk@gmail.com>
2016-07-13 23:14:08 +0300

    Updated fonts helper

--- SP/SwissPlanner/SwissPlanner/FontsHelper.m
+++ SP/SwissPlanner/SwissPlanner/FontsHelper.m
@@ -15,9 +15,9 @@
     NSString *platform = [PlatformTypeChecker platformType];
     if ([platform isEqualToString:@"iPhone 6"]||[platform isEqualToString:@"iPhone 6S"]||[platform isEqualToString:@"iPhone 6 Plus"]||[platform isEqualToString:@"iPhone 6S Plus"]) {
         return 16;
-    } else if ([platform isEqualToString:@"iPhone 4"]||[platform isEqualToString:@"iPhone 4S"]||[platform isEqualToString:@"Simulator"]){
+    } else if ([platform isEqualToString:@"iPhone 4"]||[platform isEqualToString:@"iPhone 4S"]){
         return 15;
-    } else if ([platform containsString:@"iPad"]){
+    } else if ([platform containsString:@"iPad"]||[platform isEqualToString:@"Simulator"]){
         return 19;
     }
     else {
